c8dfcd41e6bbad933b29ae1f2ee9fee40b2d0e41
Gironape <andreyka_kazakov_03@mail.ru>
2022-10-20 18:16:04 +0300

    almost the final
    an attempt to go from one site by a link to another. The main task with finding the dollar exchange rate is done

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+import csv
 from pprint import pprint
-
 import requests
+from bs4 import BeautifulSoup
+import urllib.request
 
-data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
-
+URL_DL='https://www.cbr-xml-daily.ru'
+headers={'User-Agent':"Mozilla/5.0"}
+html_page = requests.get(URL_DL, headers)
+soup = BeautifulSoup(html_page.text, "html.parser")
+search = soup.find_all(id="example")
+search=urllib
+print(search)
+data=requests.get(search).json()
 count=0
-while count<6:
-    pprint(data['Date'])
-    pprint(data['Valute']['USD']['Value'])
-    data = requests.get('https:'+data['PreviousURL']).json()
-    count+=1
+with open("dataset.csv", mode="w", encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+    file_writer.writerow(["Дата","\t""Курс Доллара"])
+    while count<6:
+        pprint(data['Date'])
+        pprint(data['Valute']['USD']['Value'])
+        file_writer.writerow([data['Date'] , data['Valute']['USD']['Value']])
+        data = requests.get('https:'+data['PreviousURL']).json()
+        count+=1
 
 
 
