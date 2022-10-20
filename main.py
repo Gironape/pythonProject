d5d635877092c2039af7cd3a90bf410f68782ec8
Gironape <andreyka_kazakov_03@mail.ru>
2022-10-20 20:56:12 +0300

    Finish
    the program copes with its task, tested

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 URL_DL='https://www.cbr-xml-daily.ru'
 headers={'User-Agent':"Mozilla/5.0"}
 html_page = requests.get(URL_DL, headers)
 soup = BeautifulSoup(html_page.text, "html.parser")
-search = soup.find_all(id="example")
-search=urllib
-print(search)
-data=requests.get(search).json()
-count=0
+data = soup.find_all(href="/daily_jsonp.js")
+data=requests.get("https://www.cbr-xml-daily.ru/daily_json.js").json()
+print("за сколько дней вы хотите увидеть курс доллара?:")
+count=1
+max=int(input())
 with open("dataset.csv", mode="w", encoding='utf-8') as w_file:
     file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
-    file_writer.writerow(["Дата","\t""Курс Доллара"])
-    while count<6:
+    file_writer.writerow(["Дата","\t\t\t\t\t\t""Курс Доллара"])
+    while count <= max:
         pprint(data['Date'])
         pprint(data['Valute']['USD']['Value'])
-        file_writer.writerow([data['Date'] , data['Valute']['USD']['Value']])
+        file_writer.writerow([data['Date'],'\t', data['Valute']['USD']['Value']])
         data = requests.get('https:'+data['PreviousURL']).json()
-        count+=1
-
+        count += 1
 
 
